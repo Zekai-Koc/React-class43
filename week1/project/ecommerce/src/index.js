6dc33c230a8126f4c03182980b548f704ac7991c
Zekai <zekaikoc@gmail.com>
2023-07-09 15:31:32 +0200

    project's netlify link added to app.js and index.js files.

--- week1/project/ecommerce/src/index.js
+++ week1/project/ecommerce/src/index.js
@@ -1,3 +1,9 @@
+//                              Project link on netlify
+// --------------------------------------------------------------------------------
+// https://64aab3117ed34f28ea2eafb1--incomparable-dieffenbachia-601ed8.netlify.app/
+//
+// --------------------------------------------------------------------------------
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
